9bc9b49094069366ac04a1f940228a1b72a77ce2
jhsaraja <jaana.h.sarajarvi@gmail.com>
2021-11-15 19:22:23 +0200

    Exercise 1.6

---
+++ osa1/unicafe/src/App.js
@@ -0,0 +1,55 @@
+import React, { useState } from 'react'
+
+const Header = ({ header }) => {
+  return (
+    <h1>{header}</h1>
+  )
+}
+
+const Display = ({ text, value }) => {
+  return (
+    <p>{text} {value}</p>
+  )
+}
+
+const Button = ({ handleClick, text }) => {
+  return (
+    <button onClick={handleClick}>
+      {text}
+    </button>
+  )
+}
+
+const App = () => {
+  // tallenna napit omaan tilaansa
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const handleGoodClick = () => {
+    setGood(good + 1)
+  }
+
+  const handleNeutralClick = () => {
+    setNeutral(neutral + 1)
+  }
+
+  const handleBadClick = () => {
+    setBad(bad + 1)
+  }
+
+  return (
+    <div>
+      <Header header="give feedback" />
+      <Button handleClick={handleGoodClick} text='good' />
+      <Button handleClick={handleNeutralClick} text='neutral' />
+      <Button handleClick={handleBadClick} text='bad' />
+      <Header header="statistics" />
+      <Display text="good" value={good} />
+      <Display text="neutral" value={neutral} />
+      <Display text="bad" value={bad} />
+    </div>
+  )
+}
+
+export default App
